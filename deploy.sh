524541cf15d52fb95ec15669d592bcd12f265413
Vinothkumar Selvaraj <vinothkumar1.selvaraj@outlook.com>
2020-07-19 15:13:54 +0530

    added final deployment changes

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,15 @@
-docker build -t vinothkumar1selvaraj/multi-client -f ./client/Dockerfile ./client
-docker build -t vinothkumar1selvaraj/multi-server -f ./server/Dockerfile ./server
-docker build -t vinothkumar1selvaraj/multi-worker -f ./worker/Dockerfile ./worker
-docker push vinothkumar1selvaraj/multi-client
-docker pust vinothkumar1selvaraj/multi-server
-docker push vinothkumar1selvaraj/multi-worker
+docker build -t vinothkumar1selvaraj/multi-client:latest -t vinothkumar1selvaraj/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t vinothkumar1selvaraj/multi-server:latest -t vinothkumar1selvaraj/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t vinothkumar1selvaraj/multi-worker:latest -t vinothkumar1selvaraj/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push vinothkumar1selvaraj/multi-client:latest
+docker pust vinothkumar1selvaraj/multi-server:latest
+docker push vinothkumar1selvaraj/multi-worker:latest
+
+docker push vinothkumar1selvaraj/multi-client:$SHA
+docker pust vinothkumar1selvaraj/multi-server:$SHA
+docker push vinothkumar1selvaraj/multi-worker:$SHA
+
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=vinothkumar1selvaraj/multi-server
+kubectl set image deployments/server-deployment server=vinothkumar1selvaraj/multi-server:$SHA
+kubectl set image deployments/client-deployment client=vinothkumar1selvaraj/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=vinothkumar1selvaraj/multi-worker:$SHA
