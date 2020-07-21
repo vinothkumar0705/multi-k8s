a31e1dd147498e0fd145cf63cf5fda45c71ab8e2
Vinothkumar Selvaraj <vinothkumar1.selvaraj@outlook.com>
2020-07-21 19:36:55 +0530

    travis iv fix

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,11 @@ docker build -t vinothkumar1selvaraj/multi-server:latest -t vinothkumar1selvaraj
 docker build -t vinothkumar1selvaraj/multi-worker:latest -t vinothkumar1selvaraj/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push vinothkumar1selvaraj/multi-client:latest
-docker pust vinothkumar1selvaraj/multi-server:latest
+docker push vinothkumar1selvaraj/multi-server:latest
 docker push vinothkumar1selvaraj/multi-worker:latest
 
 docker push vinothkumar1selvaraj/multi-client:$SHA
-docker pust vinothkumar1selvaraj/multi-server:$SHA
+docker push vinothkumar1selvaraj/multi-server:$SHA
 docker push vinothkumar1selvaraj/multi-worker:$SHA
 
 kubectl apply -f k8s
